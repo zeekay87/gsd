a9ee8a262119ea1c0c42e4d74ae7d634802b0541
zeekay87 <66121993+zeekay87@users.noreply.github.com>
2021-01-03 18:59:41 +0530

    Update app.js

--- first-container/app.js
+++ first-container/app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static('static'));
 app.use(morgan('combined'));
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8090;
 var message = process.env.MESSAGE || "Have a blast learning Docker!";
 
 app.get('/', function (req, res) {
